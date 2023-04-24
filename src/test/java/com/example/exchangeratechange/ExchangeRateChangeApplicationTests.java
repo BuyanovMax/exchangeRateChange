82cfbb783fc63f355f30e6125108ed27d4540836
Макс <maxim__90.90@mail.ru>
2023-04-24 15:58:29 +0400

    first commit

---
+++ src/test/java/com/example/exchangeratechange/ExchangeRateChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchangeratechange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateChangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
